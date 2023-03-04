501c624cddf13fe28e7ba46d9b077c612dc602ef
Oscar Rodgers <oscar@MacBook-Pro.local>
2023-03-04 13:00:08 +1100

    final touh

--- application-javascript/util.js
+++ application-javascript/util.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const myChannel = 'alpha';
+const myChannel = 'mychannel';
 const myChaincodeName = 'rps';
 
 const buildCCPOrg1 = () => {
